408d0a68e2db5fa36f2d0110e518c13ed61ce02c
Jared Kofron <jared.kofron@gmail.com>
2011-05-13 15:24:23 -0700

    first commit of handlers header.  defines headers for various signals as defined in signal.h"

---
+++ include/mantis_handlers.hpp
@@ -0,0 +1,10 @@
+#ifndef __mantis_handlers_hpp
+#define __mantis_handlers_hpp
+
+#include "mantis_status.hpp"
+#include "mantis_run_ctrl.hpp"
+#include <iostream>
+
+void abort_run(int);
+
+#endif //__mantis_handlers_hpp
