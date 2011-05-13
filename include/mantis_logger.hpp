b20c44c26116c81ca7720d2c16092a4746bec113
Jared Kofron <jared.kofron@gmail.com>
2011-05-13 15:49:55 -0700

    added a ported version of P8Log to mantis.  obviously this is mantis_logger... supports various log levels.  in the future this will be really fancy.

---
+++ include/mantis_logger.hpp
@@ -0,0 +1,28 @@
+#ifndef __mantis_logger_hpp
+#define __mantis_logger_hpp
+/* mantis_logger.hpp
+ * started by jared kofron <jared.kofron@gmail.com> 3/14/2011
+ * provides an abstract logging interface for the project 8 daq.
+ */
+
+#include <string>
+#include <iostream>
+#include <memory>
+#include <time.h>
+
+class mantis_logger
+{
+private:
+  mantis_logger();
+  ~mantis_logger();
+  static void updateTime();
+  static char fmtTimeBuf[23];
+  static time_t rawTimeBuf;
+  static struct tm *timeinfo;
+public:
+  static void Warn(std::string);
+  static void Error(std::string);
+  static void Info(std::string);
+};
+
+#endif //__mantis_logger_hpp
