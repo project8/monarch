dbd8a88b6c34768387d528fed2ffbab28799ccce
Jared Kofron <jared.kofron@gmail.com>
2011-05-12 17:07:22 -0700

    added the mantis environment parser.  allows the transformation of argc/argv into something useful to mantis. Closes #1

---
+++ include/mantis_env.hpp
@@ -0,0 +1,38 @@
+#ifndef __env_hpp
+#define __env_hpp
+
+#include "mantis_exceptions.hpp"
+#include <sstream>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <ostream>
+#include <getopt.h>
+#include <ctime>
+
+class mantis_env;
+typedef std::auto_ptr<mantis_env> safeEnvPtr;
+
+class mantis_env
+{
+
+private:
+  std::string outName;
+  double clockRate;
+  std::size_t runLength;
+public:
+  mantis_env();
+  static safeEnvPtr parseArgs(int argc,char** argv);
+  static void verifyEnvironment(safeEnvPtr);
+  void setOutName(std::string);
+  std::string getOutName();
+  void setClockRate(std::string);
+  double getClockRate();
+  void setRunLength(std::string);
+  std::size_t getRunLength();
+
+  friend std::ostream& operator << (std::ostream& outstream, 
+				    safeEnvPtr);
+};
+
+#endif
