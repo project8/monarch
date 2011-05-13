3538f039c6413396376180e2d4bd234bec2ba0b3
Jared Kofron <jared.kofron@gmail.com>
2011-05-13 15:34:40 -0700

    added a little formatting

--- src/mantis_handlers.cpp
+++ src/mantis_handlers.cpp
@@ -4,11 +4,11 @@ void abort_run(int condition)
 {
   switch(condition) {
   case run_end_normal:
-    std::cout << "run ended normally.  cleaning up and terminating..."
+    std::cout << "\n\trun ended normally.  cleaning up and terminating..."
 	      << std::endl;
     break;
   case run_end_killed:
-    std::cout << "run termination requested.  cleaning up..."
+    std::cout << "\n\trun termination requested.  cleaning up..."
 	      << std::endl;
     break;
   default:
