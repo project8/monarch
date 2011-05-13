6ccf8529ddbd2d89faa91c18b94614c94e49466e
Jared Kofron <jared.kofron@gmail.com>
2011-05-13 15:25:02 -0700

    added #ifdef to prevent error from multiple inclusion

--- include/mantis_status.hpp
+++ include/mantis_status.hpp
@@ -1,1 +1,8 @@
+#ifndef __mantis_status_hpp
+#define __mantis_status_hpp
+
 static const int env_arg_error = 1;
+static const int run_end_normal = 0;
+static const int run_end_killed = 2;
+
+#endif //__mantis_status_hpp
