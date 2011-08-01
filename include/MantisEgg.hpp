11c0027559ff85c6b8988d7e388d4889d71497fd
Jared Kofron <jared.kofron@gmail.com>
2011-08-01 03:19:43 +0000

    switched the header format to conform to actual XML standards as pointed out by Lisa. now each element actually has a list of attributes, and you can add to an existing list (or create a new one) by doing egg->add_header_attr(element_key,attribute_key,attribute_value).

--- include/MantisEgg.hpp
+++ include/MantisEgg.hpp
@@ -4,16 +4,20 @@
 #include "MantisData.hpp"
 #include "MantisEnv.hpp"
 #include <cstdio>
+#include <list>
 #include <string>
 #include <cstring>
 #include <utility>
 #include <sstream>
 #include <map>
 
-typedef std::string egg_hdr_k_type;
-typedef std::string egg_hdr_v_type;
-typedef std::pair<egg_hdr_k_type,egg_hdr_v_type> egg_hdr_type;
-typedef std::map<egg_hdr_k_type,egg_hdr_v_type> egg_hdr_container;
+typedef std::string egg_hdr_elem_k_t;
+typedef std::string egg_hdr_attr_k_t;
+typedef std::string egg_hdr_attr_v_t;
+typedef std::pair<egg_hdr_attr_k_t,egg_hdr_attr_v_t> egg_hdr_attr_t;
+typedef std::list<egg_hdr_attr_t> egg_hdr_attr_list;
+typedef std::map<egg_hdr_elem_k_t,egg_hdr_attr_list> egg_hdr_container;
+typedef std::pair<egg_hdr_elem_k_t,egg_hdr_attr_list> egg_hdr_elem_t;
 
 class MantisEgg {
 private:
@@ -21,8 +25,8 @@ private:
   bool header_finished;
   egg_hdr_container header_attrs;
   std::string file_name;
-  static std::string attr_to_xml(egg_hdr_k_type key,
-				 egg_hdr_v_type val);
+  static std::string attr_to_xml(egg_hdr_attr_k_t key,
+				 egg_hdr_attr_v_t val);
   static std::string xml_vers_header();
   static std::string xml_hdr_open();
   static std::string xml_hdr_close();
@@ -35,8 +39,10 @@ private:
 public:
   static MantisEgg* egg_from_env(safeEnvPtr& env);
 
-  template<typename T> bool add_header_attr(egg_hdr_k_type key, 
-					    T value);
+  template<typename T1,typename T2> 
+  bool add_header_attr(egg_hdr_elem_k_t key, 
+		       T1 value1,
+		       T2 value2);
   bool write_header();
   bool write_data(MantisData* block);
   ~MantisEgg();
@@ -48,16 +54,34 @@ union serializer {
   unsigned char value_bytes[sizeof(T)];
 };
 
-template<typename T>
-bool MantisEgg::add_header_attr(egg_hdr_k_type key,
-				 T value)
+template<typename T1, typename T2>
+bool MantisEgg::add_header_attr(egg_hdr_elem_k_t key,
+				T1 value1,
+				T2 value2)
 {
-  std::pair<egg_hdr_container::iterator,bool> ret;
-  std::stringstream converter;
+  egg_hdr_attr_t new_attr;
+  egg_hdr_attr_list new_attr_list;
 
-  converter << value;
-  ret = (*this).header_attrs.insert(egg_hdr_type(key,converter.str()));
-  return ret.second;
+  std::stringstream converter;
+  converter << value1;
+  new_attr.first = converter.str();
+  converter.str(std::string());
+  converter << value2;
+  new_attr.second = converter.str();
+
+  new_attr_list.push_back(new_attr);
+
+  // Search the map for the key.  If it exists, merge
+  // our new list with the existing list.  Otherwise,
+  // insert the new value.
+  if( (*this).header_attrs.count(key) == 0 ) {
+    (*this).header_attrs[key] = new_attr_list;
+  }
+  else {
+    (*this).header_attrs[key].merge(new_attr_list);
+  }
+
+  return true;
 }
 
 #endif
