d8d4b5032a7bdb23fca9e50cafe234ed41f3ee4a
Jared Kofron <jared.kofron@gmail.com>
2012-07-25 16:12:47 -0700

    implementation of EndOfFile exception

--- src/MonarchExceptions.cpp
+++ src/MonarchExceptions.cpp
@@ -3,3 +3,7 @@
 MonarchExceptions::NoFile::NoFile (std::string const& why) 
   : std::logic_error(why)
 {}
+
+MonarchExceptions::EndOfFile::EndOfFile (std::string const& why) 
+  : std::logic_error(why)
+{}
