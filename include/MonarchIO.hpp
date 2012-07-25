d8b234760267712904c8dc317a211222efc034f3
Jared Kofron <jared.kofron@gmail.com>
2012-07-25 16:10:31 -0700

    MonarchIO no longer knows about records (this makes more conceptual sense)

--- include/MonarchIO.hpp
+++ include/MonarchIO.hpp
@@ -10,18 +10,23 @@
 class MonarchIO {
   FILE *filePTR;
   AccessMode _iomode;
-  std::size_t _recWidth;
 public:
   // Access Modes
   static const AccessMode WriteMode = 0x01;
   static const AccessMode ReadMode  = 0x02;
   
   // Constructors and Destructors
-  MonarchIO(std::string filename, 
-	    AccessMode iomode,
-	    std::size_t nbytes);
+  MonarchIO(std::string filename,
+	    AccessMode iomode);
   ~MonarchIO();
 
   // API
-  bool WriteRecord(MonarchRecord* data);
+  
+  // Write nbytes of data from the byte array wbuf to the
+  // current position of the file pointer.
+  bool Write(unsigned char* wbuf, std::size_t nbytes);
+
+  // Read nbytes of data from the file pointer and store
+  // the result in the byte array rbuf.
+  bool Read(unsigned char* rbuf, std::size_t nbytes);
 };
