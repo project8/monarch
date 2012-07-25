7c33c2b5de5f2983e65ba70735b0da3456b4a646
Jared Kofron <jared.kofron@gmail.com>
2012-07-25 16:12:13 -0700

    added forward declarations for GetNextEvent, WriteRecord, Close, and NewRecord.  also messing around with getting the serialization to work

--- include/monarch.hpp
+++ include/monarch.hpp
@@ -3,11 +3,16 @@
 
 #include "MonarchIO.hpp"
 #include "MonarchTypes.hpp"
+#include "MonarchExceptions.hpp"
 
 class Monarch {
 private:
   // Default constructor is private, naturally
   Monarch();
+
+  // Filename we are currently working with
+  std::string filename;
+  std::size_t recsize;
 
   // The MonarchIO class is the file pointer abstraction that we use
   // to read and write data.
@@ -17,6 +22,7 @@ private:
   // the MonarchRecord type.  
   char* const mem;
   MonarchSerializer<MonarchRecord>* const rec;
+  MonarchSerializer<MonarchRecord>* ser;
 
   // The simplest constructor takes a string filename, an access mode, and
   // an acquisition mode which determines the number of channels in the data
@@ -28,6 +34,7 @@ private:
 
   // Specialized open methods
   static Monarch* OpenForReading(std::string filename);
+  static Monarch* OpenForWriting(std::string filename);
 
 public:
   // Some bits for configuration flags.
@@ -42,6 +49,22 @@ public:
   // The most basic constructor only needs a filename and an access mode.
   // In read mode this assumes one channel!
   static Monarch* Open(std::string filename, AccessMode iomode);
+
+  // Close an open monarch file.
+  bool Close();
+
+  // A RecordFactory - a factory method which builds properly initialized 
+  // MonarchRecords.
+  static MonarchRecord* NewRecord(std::size_t rsize);
+
+  // Write a MonarchRecord to disk.  Assumes that the record is the right size!
+  // To avoid disappointment, generate your records with Monarch::NewRecord.
+  bool WriteRecord(MonarchRecord* rec);
+
+  // Read the next event on disk into the MonarchSerializer buffer and return
+  // a pointer to the value member of the union.  This exposes a MonarchRecord*
+  // to the client.
+  MonarchRecord* GetNextEvent();
 
 };
 
