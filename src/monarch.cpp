9e438f298537c7533548e96619a1220d2a78b3ff
Jared Kofron <jared.kofron@gmail.com>
2012-07-25 16:13:52 -0700

    working on this, esp. WriteRecord

--- src/monarch.cpp
+++ src/monarch.cpp
@@ -1,7 +1,7 @@
 #include "Monarch.hpp"
 
 Monarch::Monarch()
-  : io(NULL), mem(NULL), rec(NULL)
+  : filename(""), io(NULL), mem(NULL), rec(NULL)
 {
   //empty, just here to make the compiler throw a
   //fit if someone tries to call a default constructor
@@ -12,7 +12,9 @@ Monarch::Monarch(std::string filename,
 		 AcquisitionMode datamode,
 		 std::size_t DataWidth) 
   : 
-  io( new MonarchIO(filename,iomode,DataWidth) ),
+  filename(filename),
+  recsize(DataWidth),
+  io( new MonarchIO(filename,iomode) ),
   mem(new char[DataWidth]), 
   rec( new (mem) MonarchSerializer<MonarchRecord>() )
 { }
@@ -24,6 +26,11 @@ Monarch* Monarch::Open(std::string filename,
   if(iomode == MonarchIO::ReadMode) {
     return Monarch::OpenForReading(filename);
   }
+  else if(iomode == MonarchIO::WriteMode) {
+    return Monarch::OpenForWriting(filename);
+  }
+
+  return NULL;
 }
 
 Monarch* Monarch::Open(std::string filename, 
@@ -32,5 +39,36 @@ Monarch* Monarch::Open(std::string filename,
 }
 
 Monarch* Monarch::OpenForReading(std::string filename) {
-  return new Monarch(filename, MonarchIO::ReadMode, OneChannel, 1024);
+  std::size_t rsize = 1024 + sizeof(MonarchRecord) - sizeof(DataType*);
+  return new Monarch(filename, MonarchIO::ReadMode, OneChannel, rsize);
+}
+
+Monarch* Monarch::OpenForWriting(std::string filename) {
+  std::size_t rsize = 1024 + sizeof(MonarchRecord) - sizeof(DataType*);
+  return new Monarch(filename, MonarchIO::WriteMode, OneChannel, rsize);
+}
+
+bool Monarch::Close() {
+  return true;
+}
+
+MonarchRecord* Monarch::NewRecord(std::size_t dsize) {
+  std::size_t rsize = dsize + sizeof(MonarchRecord) - sizeof(DataType*);
+  unsigned char* mem = new unsigned char[rsize]();
+  return new (mem) MonarchRecord();
+}
+
+bool Monarch::WriteRecord(MonarchRecord* newrec) {
+  (this->rec->value_bytes) = (unsigned char*)(newrec);
+  return (this->io)->Write((this->rec->value_bytes), this->recsize);
+}
+
+MonarchRecord* Monarch::GetNextEvent() {
+  bool res = (this->io)->Read(this->rec->value_bytes,this->recsize);
+  if( res == true ) {
+    return &(this->rec->value);
+  }
+  else {
+    throw MonarchExceptions::EndOfFile(this->filename);
+  }
 }
