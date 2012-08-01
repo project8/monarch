4e51fbc71fb68f805732b7fd4e7b2a936497fc95
Jared Kofron <jared.kofron@gmail.com>
2012-08-01 10:32:11 -0700

    unsigned char -> char, let's all be cool here

--- include/MonarchIO.hpp
+++ include/MonarchIO.hpp
@@ -24,11 +24,11 @@ public:
   
   // Write nbytes of data from the byte array wbuf to the
   // current position of the file pointer.
-  bool Write(unsigned char* wbuf, std::size_t nbytes);
+  bool Write(char* wbuf, std::size_t nbytes);
 
   // Read nbytes of data from the file pointer and store
   // the result in the byte array rbuf.
-  std::size_t Read(unsigned char* rbuf, std::size_t nbytes);
+  std::size_t Read(char* rbuf, std::size_t nbytes);
 
   // Close the file handle owned by this IO object
   bool Close();
