8ae9c91fa30229ff6c934f8fa05c4b8edb17c7a6
Jared Kofron <jared.kofron@gmail.com>
2012-08-06 10:43:46 -0700

    binary 0bxx syntax is only supported by gcc > 4.2, so we need to check and define in terms of hex if 0b is not available

--- include/MonarchUtils.hpp
+++ include/MonarchUtils.hpp
@@ -5,7 +5,21 @@
 typedef char byte;
 
 // Bitmasks for options
+#if __GNUC__ == 4 && __GNUC_MINOR__ < 3 
 
+// Was the input file specified?
+byte ifile_set = 0x01;
+
+// Are we supposed to pretty print the header?
+byte pp_header = 0x01 << 1;
+
+// Are we supposed to extract the header?
+byte ex_header = 0x01 << 2;
+
+// Are we dumping the header to a file?
+byte hdr_o_file = 0x01 << 3;
+
+#else
 // Was the input file specified?
 byte ifile_set = 0b00000001;
 
@@ -17,6 +31,8 @@ byte ex_header = 0b00000100;
 
 // Are we dumping the header to a file?
 byte hdr_o_file = 0b00001000;
+
+#endif
 
 // Check if a bit is set in a bitmask
 bool bit_set(byte tgt, byte query) {
