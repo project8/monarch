920101d7b7e37e171820a0abfbee52d7dac66c2f
Dan Furse <dlfurse@mit.edu>
2012-08-30 22:35:46 -0700

    forgot the file, jesus christ

---
+++ src/MonarchInfo.cpp
@@ -0,0 +1,40 @@
+#include "Monarch.hpp"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main( const int argc, const char** argv )
+{
+    const Monarch* tReadTest = Monarch::OpenForReading( argv[1] );
+
+    if( tReadTest->ReadHeader() == false )
+    {
+        cout << "could not read header!" << endl;
+        return -1;
+    }
+    const MonarchHeader* tReadHeader = tReadTest->GetHeader();
+    cout << "acquisition mode <" << tReadHeader->GetAcqMode() << ">" << endl;
+    cout << "acquisition rate <" << tReadHeader->GetAcqRate() << ">" << endl;
+    cout << "acquisition time <" << tReadHeader->GetAcqTime() << ">" << endl;
+    cout << "record size <" << tReadHeader->GetRecordSize() << ">" << endl;
+
+    unsigned int tRecordCount = 0;
+    unsigned int tAcquisiontCount = 0;
+    const MonarchRecord* tReadRecord = tReadTest->GetRecord();
+    while( tReadTest->ReadRecord() != false )
+    {
+        tRecordCount = tRecordCount + 1;
+        if( tReadRecord->fAId == tAcquisiontCount )
+        {
+            tAcquisiontCount = tAcquisiontCount + 1;
+        }
+    }
+    cout << "record count <" << tRecordCount << ">" << endl;
+    cout << "acquisition count <" << tAcquisiontCount << ">" << endl;
+
+    tReadTest->Close();
+    delete tReadTest;
+
+    return 0;
+}
