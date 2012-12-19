5c97980daf54b8a2e2782455327784867d4de7d7
Dan Furse <dlfurse@mit.edu>
2012-12-19 15:58:33 -0500

    DO NOT USE -- debugging

--- Source/MonarchRecord.cpp
+++ Source/MonarchRecord.cpp
@@ -1,10 +1,6 @@
 #include "MonarchRecord.hpp"
 
-MonarchRecord::MonarchRecord() :
-     fAId( 0 ),
-     fRId( 0 ),
-     fTick( 0 ),
-     fDataPtr( NULL )
+MonarchRecord::MonarchRecord()
 {
 }
 MonarchRecord::~MonarchRecord()
