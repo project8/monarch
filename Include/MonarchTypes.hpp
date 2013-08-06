fe008be19d2bef117e09d5a9db656971e265307e
Noah Oblath <nsoblath@mit.edu>
2013-08-06 16:59:33 -0400

    Added is_same test

--- Include/MonarchTypes.hpp
+++ Include/MonarchTypes.hpp
@@ -2,40 +2,56 @@
 #define __MONARCH_TYPES_HPP
 
 // We need clock_t
+#include <stdint.h>
 #include <ctime>
 #include <string>
 
+template<typename T, typename U>
+struct is_same
+{
+    static const bool value = false;
+};
+
+template<typename T>
+struct is_same<T,T>  //specialization
+{
+   static const bool value = true;
+};
+
+template<bool> struct static_assert {};
+template<> struct static_assert<true> {};  //specialization
+
 typedef size_t PreludeType;
 
-typedef unsigned int AccessModeType;
+typedef uint32_t AccessModeType;
 static const AccessModeType sAccessRead = 0;
 static const AccessModeType sAccessWrite = 1;
 
-typedef unsigned int InterfaceModeType;
+typedef uint32_t InterfaceModeType;
 static const AccessModeType sInterfaceInterleaved = 0;
 static const AccessModeType sInterfaceSeparate = 1;
 
-typedef unsigned int AcquisitionModeType;
+typedef uint32_t AcquisitionModeType;
 static const AcquisitionModeType sOneChannel = 1;
 static const AcquisitionModeType sTwoChannel = 2;
 
-typedef unsigned int RunType;
+typedef uint32_t RunType;
 static const RunType sRunTypeSignal = 0;
 static const RunType sRunTypeBackground = 1;
 static const RunType sRunTypeOther = 999;
 
-typedef unsigned int RunSourceType;
+typedef uint32_t RunSourceType;
 static const RunSourceType sSourceMantis = 0;
 static const RunSourceType sSourceSimulation = 1;
 
-typedef unsigned int FormatModeType;
+typedef uint32_t FormatModeType;
 static const FormatModeType sFormatSingle = 0;
 static const FormatModeType sFormatMultiSeparate = 1;
 static const FormatModeType sFormatMultiInterleaved = 2;
 
-typedef unsigned long int AcquisitionIdType; // 8 bytes
-typedef unsigned long int RecordIdType; // 8 bytes
-typedef long long int TimeType; // 8 bytes
+typedef uint64_t AcquisitionIdType; // 8 bytes
+typedef uint64_t RecordIdType; // 8 bytes
+typedef uint64_t TimeType; // 8 bytes
 typedef unsigned char DataType;
 
 static const std::string sDateTimeFormat("%Y-%m-%d %H:%M:%S %z");
