381ddcc675e84e7b2fa98658c356a1d5022d6d9d
Project 8 @ Irene <projecteight@gmail.com>
2015-01-31 19:17:42 -0800

    Split M3Types.hh into M3Types.hh and M3Constants.hh, and added API macros for Windows

---
+++ Monarch3/M3Constants.hh
@@ -0,0 +1,47 @@
+/*
+ * MConstants.hh
+ *
+ *  Created on: Jan 31, 2015
+ *      Author: nsoblath
+ */
+
+#ifndef M3CONSTANTS_HH_
+#define M3CONSTANTS_HH_
+
+#include "thorax.hh"
+
+namespace monarch3
+{
+    // API export macros for windows
+#ifdef _WIN32
+#  ifdef M3_API_EXPORTS
+#    define M3_API __declspec(dllexport)
+#  else
+#    define M3_API __declspec(dllimport)
+#  endif
+#else
+#  define M3_API
+#endif
+
+    // channel information
+
+    /// Specifies whether the data is digitized or analog
+    typedef unsigned DataFormatType;
+    static const DataFormatType sDigitized = 0;
+    static const DataFormatType sAnalog = 1;
+
+    // stream information
+
+    /// Specifies whether the data channels are interleaved or separate in a stream
+    typedef uint32_t MultiChannelFormatType;
+    static const MultiChannelFormatType sInterleaved = 0;
+    static const MultiChannelFormatType sSeparate = 1;
+
+    // re-typdefing for aesthetic purposes
+    typedef acquisition_id_type AcquisitionIdType; // 8 bytes
+    typedef record_id_type RecordIdType; // 8 bytes
+    typedef time_nsec_type TimeType; // 8 bytes
+
+}
+
+#endif // M3CONSTANTS_HH_
