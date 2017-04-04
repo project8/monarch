29d8bb4d18ce272df391873e9ac2d18a12b0c1ac
Noah Oblath <nsoblath@gmail.com>
2017-04-04 10:56:59 -0700

    Inline a function and make an explicit conversion from bool to int.

--- Monarch3/M3Stream.cc
+++ Monarch3/M3Stream.cc
@@ -316,7 +316,7 @@ namespace monarch3
 
         std::unique_lock< std::mutex >( *fMutexPtr.get() );
 
-        anOffset += fRecordsAccessed;
+        anOffset += (int)fRecordsAccessed;
         if( ( anOffset < 0 && (unsigned)abs( anOffset ) > fRecordCountInFile ) ||
             ( anOffset > 0 && fRecordCountInFile + anOffset >= fNRecordsInFile ) ||
             ( anOffset == 0 && fNRecordsInFile == 0 ))
@@ -371,11 +371,6 @@ namespace monarch3
         return;
     }
 
-
-    M3Record* M3Stream::GetStreamRecord()
-    {
-        return &fStreamRecord;
-    }
 
     M3Record* M3Stream::GetChannelRecord( unsigned aChannel )
     {
